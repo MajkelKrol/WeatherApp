01afe643234d7d0e3d546ef6e82825fa98d2ea83
iMax <majkelkrol@icloud.com>
2020-11-30 13:30:44 +0100

    Add ZStack, VStack and Button

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -10,7 +10,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Cupertino, CA")
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                Spacer()
+                Image(systemName: "cloud.sun.fill")
+                .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, height: 190, alignment: .center)
+                Text("76*")
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 70, weight: .medium))
+                Spacer()
+                HStack {
+                    VStack {
+                        Text("TUE")
+                        Image(systemName: "cloud.sun.fill")
+                        Text("76*")
+                    }
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    print("add")
+                }) {
+                    Text("Change Time of Day")
+                        .font(.title)
+                        .padding()
+                        .background(Color.white)
+                        .foregroundColor(Color.black)
+                        .cornerRadius(20)
+                }
+                Spacer()
+            }
+        }
     }
 }
 
